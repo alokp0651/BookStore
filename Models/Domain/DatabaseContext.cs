224533ce8b3f78b2b529b3e150da08c7396aa8b3
alokd <alokd@KNOXY>
2024-02-22 21:03:14 +0530

    Add project files.

---
+++ Models/Domain/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BookStore.Models.Domain
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext>options): base(options) 
+        {
+             
+        }
+        public DbSet<Genre> Genre { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Publisher> Publisher {  get; set; }
+        public DbSet<Book> Book { get; set; }
+        
+    }
+}
